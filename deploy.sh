cb9bc243fe6374ad5fe1abf32158459db5ccd092
KPCCoiL <copernicium@me.com>
2019-05-15 15:08:52 +0900

    only deploy dev-main.js

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -e
 rm -rf target/public
 clojure -A:fig:min
 cp -a resources/public/* docs/
-cp -a target/public/cljs-out docs/
+cp -a target/public/cljs-out/dev-main.js docs/cljs-out/
 git add docs/*
 git commit -m "deploy"
 git push
