3c7a9a3811530368b000dc886b7c92ffd60a78d9
KPCCoiL <copernicium@me.com>
2019-05-18 12:00:55 +0900

    remove comment

--- src/tsglive_shooting/core.cljs
+++ src/tsglive_shooting/core.cljs
@@ -19,14 +19,8 @@
   (when-let [el (get-app-element)]
     (mount el)))
 
-;; conditionally start your application based on the presence of an "app" element
-;; this is particularly helpful for testing this ns without launching the app
 (mount-app-element)
 
-;; specify reload hook with ^;after-load metadata
 (defn ^:after-load on-reload []
   (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
