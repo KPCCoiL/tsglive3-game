8c8d0ea6e37f7607030f52d3616df16ce3c9fb27
KPCCoiL <copernicium@me.com>
2019-05-18 12:06:09 +0900

    create screen

--- src/tsglive_shooting/core.cljs
+++ src/tsglive_shooting/core.cljs
@@ -3,17 +3,34 @@
    [goog.dom :as gdom]
    [rum.core :as rum]))
 
+(def width 300)
+(def height 600)
+(def ship-size 30)
+
+(rum/defc screen []
+  [:div
+   [:svg {:x 0
+          :y 0
+          :width width
+          :height height}
+    [:rect {:x 0
+            :y 0
+            :width width
+            :height height
+            :stroke "black"
+            :fill "white"
+            :stroke-width 2}]
+    [:image {:x 150
+             :y 300
+             :href "images/spaceship.svg"
+             :width ship-size
+             :height ship-size}]]])
 
 (defn get-app-element []
   (gdom/getElement "app"))
 
-(rum/defc hello-world []
-  [:div
-   [:h1 "TSG LIVE! 3 live game coding"]
-   [:h3 "Here comes the game!"]])
-
 (defn mount [el]
-  (rum/mount (hello-world) el))
+  (rum/mount (screen) el))
 
 (defn mount-app-element []
   (when-let [el (get-app-element)]
